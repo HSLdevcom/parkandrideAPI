88e769b4c3114f67e40038d03f928c3ca7f47fac
Isto Nikula <isto.nikula@iki.fi>
2014-08-28 17:48:17 +0300

    chore(frontend): bundle ui resources into deployable
    See http://spring.io/blog/2013/12/19/serving-static-web-content-with-spring-boot

--- application/src/main/frontend/build.config.js
+++ application/src/main/frontend/build.config.js
@@ -8,7 +8,7 @@ module.exports = {
    * completely built.
    */
   build_dir: 'build',
-  compile_dir: 'bin',
+  compile_dir: 'bin/static',
 
   /**
    * This is a collection of file patterns that refer to our app code (the

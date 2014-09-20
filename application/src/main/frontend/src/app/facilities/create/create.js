e000dd812fc56090ea27129a7925f51db246eef9
Isto Nikula <isto.nikula@iki.fi>
2014-09-20 20:29:57 +0300

    feat(ngFacility): add create facillity from prototype

--- application/src/main/frontend/src/app/facilities/create/create.js
+++ application/src/main/frontend/src/app/facilities/create/create.js
@@ -1,10 +1,10 @@
-angular.module('ngBoilerplate.facilities.create', [
+angular.module('facilities.create', [
     'ui.router'
 ])
 
     .config(function config($stateProvider) {
-        $stateProvider.state('facilities.create', {
-            url: '/facilities/create',
+        $stateProvider.state('facilities-create', { // dot notation in ui-router indicates nested ui-view
+            url: '/facilities/create', // TODO set facilities base path on upper level and say here /create ?
             views: {
                 "main": {
                     controller: 'CreateCtrl',

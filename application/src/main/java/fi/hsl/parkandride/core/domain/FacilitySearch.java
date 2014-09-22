1fc5c68ca2c145c2e6d6863b9a48d1e6140b07a2
Samppa Saarela <samppa.saarela@iki.fi>
2014-09-22 07:52:53 +0300

    fix(tests): src/main/resources as resources dir. Facilities paging.

--- application/src/main/java/fi/hsl/parkandride/core/domain/FacilitySearch.java
+++ application/src/main/java/fi/hsl/parkandride/core/domain/FacilitySearch.java
@@ -6,8 +6,33 @@ public class FacilitySearch {
 
     public int limit = 100;
 
-    public long offset = 0;
+    public long offset = 0l;
 
     public Polygon intersecting;
 
+    // Getters and setters for Spring data binder
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
+
+    public long getOffset() {
+        return offset;
+    }
+
+    public void setOffset(long offset) {
+        this.offset = offset;
+    }
+
+    public Polygon getIntersecting() {
+        return intersecting;
+    }
+
+    public void setIntersecting(Polygon intersecting) {
+        this.intersecting = intersecting;
+    }
 }

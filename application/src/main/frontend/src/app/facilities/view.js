1bcfcc3ee0aacca988509aeb5dc1b042f75514e4
Isto Nikula <isto.nikula@iki.fi>
2014-09-22 13:45:33 +0300

    feat(ngFacilities): navigation from list to view

--- application/src/main/frontend/src/app/facilities/view.js
+++ application/src/main/frontend/src/app/facilities/view.js
@@ -1,15 +1,22 @@
 (function() {
     var m = angular.module('facilities.view', [
-        'ui.router'
+        'ui.router',
+        'restangular'
     ]);
 
     m.config(function config($stateProvider) {
         $stateProvider.state('facilities-view', { // dot notation in ui-router indicates nested ui-view
-            url: '/facilities/view', // TODO set facilities base path on upper level and say here /create ?
+            url: '/facilities/view/:id', // TODO set facilities base path on upper level and say here /create ?
             views: {
                 "main": {
                     controller: 'ViewCtrl',
-                    templateUrl: 'facilities/view.tpl.html'
+                    templateUrl: 'facilities/view.tpl.html',
+                    resolve: {
+                        facility: function($stateParams, Restangular) {
+                            // TODO put to service
+                            return Restangular.one('facilities', $stateParams.id).get();
+                        }
+                    }
                 }
             },
             data: { pageTitle: 'View Facility' }
@@ -17,5 +24,8 @@
     });
 
     m.controller('ViewCtrl', ViewController);
-    function ViewController() {}
+    function ViewController(facility, $log) {
+        this.facility = facility;
+        $log.info(facility);
+    }
 })();

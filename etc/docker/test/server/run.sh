7003a06338b033a71468ce7c05a5a11621973feb
Isto Nikula <isto.nikula@iki.fi>
2014-09-26 23:37:30 +0300

    chore(deploy): update test server docker run script
    - run as daemon
    - use explicit name

--- etc/docker/test/server/run.sh
+++ etc/docker/test/server/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -v `pwd`:/data/parkandride/ -d -P 8080:8080 parkandrideapi/server
+docker run -d -p 8080:8080 -v `pwd`:/data/parkandride/ --name parkandrideapi-server parkandrideapi/server

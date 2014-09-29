c1b6c2b679ce2e6d55a4a201622a7f9cebb4a6ce
Isto Nikula <isto.nikula@iki.fi>
2014-09-29 22:05:57 +0300

    refactor(deploy): reorganize deploy

---
+++ etc/gocd/build/build-release.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -eu
+: ${GO_PIPELINE_COUNTER:?}
+: ${PWD:?}
+set -x
+
+function version() {
+  mvn -Dexec.executable="echo" \
+    -Dexec.args='${project.version}' \
+    -Dexec.outputFile="version" \
+    --non-recursive \
+    --batch-mode \
+    org.codehaus.mojo:exec-maven-plugin:1.3.1:exec >/dev/null 2>&1 ||
+    { echo "Unable to determine version" 1>&2; exit 1; }
+
+  sed -i -e "s/SNAPSHOT$/$GO_PIPELINE_COUNTER/" version
+  cat version
+}
+
+function revision() {
+  echo `git rev-parse HEAD` > revision
+  cat revision
+}
+
+VERSION=`version`
+REVISION=`revision`
+
+mvn versions:set \
+    --batch-mode \
+    --errors \
+    -DgenerateBackupPoms=false \
+    -DnewVersion="$VERSION" \
+    --file parent/pom.xml
+
+mvn clean deploy \
+    --batch-mode \
+    --errors \
+    -DaltDeploymentRepository="staging::default::file:staging"

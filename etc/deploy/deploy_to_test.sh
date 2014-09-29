24d20a6d5814f0b0d1956f70dce04d416cfbe1c7
Isto Nikula <isto.nikula@iki.fi>
2014-09-29 12:45:07 +0300

    chore(deploy): add deploy script

---
+++ etc/deploy/deploy_to_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -x
+
+PARKANDRIDE_VERSION=0.0.1-SNAPSHOT
+
+AWS_USER=ubuntu
+AWS_HOST=54.171.6.108
+
+IDENTITY_FILE=/var/go/hsl-liipi.pem
+
+# transfer
+scp -r -i $IDENTITY_FILE etc $AWS_USER@$AWS_SERVER:
+scp -r -i $IDENTITY_FILE parkandride-application-$VERSION.jar $AWS_USER@$AWS_SERVER:parkandride-application-latest.jar
+
+# stop and start docker container, build new images and restart containers

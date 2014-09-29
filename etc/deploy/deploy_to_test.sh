ad40f0e0baa3873e0b3ee894425eecd8ec85af01
Isto Nikula <isto.nikula@iki.fi>
2014-09-29 13:50:22 +0300

    chore(deploy): add deploy script, fix host name

--- etc/deploy/deploy_to_test.sh
+++ etc/deploy/deploy_to_test.sh
@@ -10,7 +10,7 @@ AWS_HOST=54.171.6.108
 IDENTITY_FILE=/var/go/hsl-liipi.pem
 
 # transfer
-scp -r -i $IDENTITY_FILE etc $AWS_USER@$AWS_SERVER:
+scp -r -i $IDENTITY_FILE etc $AWS_USER@$AWS_HOST:
 scp -r -i $IDENTITY_FILE parkandride-application-$VERSION.jar $AWS_USER@$AWS_SERVER:parkandride-application-latest.jar
 
 # stop and start docker container, build new images and restart containers

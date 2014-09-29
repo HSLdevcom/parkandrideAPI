ce3d304ab6dc878b7b905dc3689f02e83a18959f
Isto Nikula <isto.nikula@iki.fi>
2014-09-29 14:36:22 +0300

    chore(deploy): add restart script

--- etc/deploy/deploy_to_test.sh
+++ etc/deploy/deploy_to_test.sh
@@ -8,9 +8,12 @@ AWS_USER=ubuntu
 AWS_HOST=54.171.6.108
 
 IDENTITY_FILE=/var/go/hsl-liipi.pem
+SCRIPT_DIR=`dirname $0`
+ETC_DIR=$SCRIPT_DIR/..
 
 # transfer
-scp -oStrictHostKeyChecking=no -r -i $IDENTITY_FILE etc $AWS_USER@$AWS_HOST:
+scp -oStrictHostKeyChecking=no -r -i $IDENTITY_FILE $ETC_DIR $AWS_USER@$AWS_HOST:
 scp -r -i $IDENTITY_FILE parkandride-application-$VERSION.jar $AWS_USER@$AWS_HOST:parkandride-application-latest.jar
 
-# stop and start docker container, build new images and restart containers
+# stop and start docker container
+ssh -i $IDENTITY_FILE -t "$AWS_USER@$AWS_SERVER" "$SCRIPT_DIR/restart.sh"
---
+++ etc/deploy/restart.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -x
+
+docker stop parkandrideapi-server && docker start parkandrideapi-server

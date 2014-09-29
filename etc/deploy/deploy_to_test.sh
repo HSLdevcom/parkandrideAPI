5a69df384917e44c8a20c7ca4c507de3066cde47
Isto Nikula <isto.nikula@iki.fi>
2014-09-29 14:08:09 +0300

    chore(deploy): add deploy script, fix version

--- etc/deploy/deploy_to_test.sh
+++ etc/deploy/deploy_to_test.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-PARKANDRIDE_VERSION=0.0.1-SNAPSHOT
+VERSION=0.0.1-SNAPSHOT
 
 AWS_USER=ubuntu
 AWS_HOST=54.171.6.108
@@ -10,7 +10,7 @@ AWS_HOST=54.171.6.108
 IDENTITY_FILE=/var/go/hsl-liipi.pem
 
 # transfer
-scp -r -i $IDENTITY_FILE etc $AWS_USER@$AWS_HOST:
+scp -oStrictHostKeyChecking=no -r -i $IDENTITY_FILE etc $AWS_USER@$AWS_HOST:
 scp -r -i $IDENTITY_FILE parkandride-application-$VERSION.jar $AWS_USER@$AWS_HOST:parkandride-application-latest.jar
 
 # stop and start docker container, build new images and restart containers

afaf9cf4bcad2bb1b3911b01f848c73e18f6bf6a
Isto Nikula <isto.nikula@iki.fi>
2014-09-29 14:55:12 +0300

    chore(deploy): add restart script, fix paths

--- etc/deploy/deploy_to_test.sh
+++ etc/deploy/deploy_to_test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# NOTE: this script is expectd to be run from project checkout dir
+
 set -x
 
 VERSION=0.0.1-SNAPSHOT
@@ -8,12 +10,10 @@ AWS_USER=ubuntu
 AWS_HOST=54.171.6.108
 
 IDENTITY_FILE=/var/go/hsl-liipi.pem
-SCRIPT_DIR=`dirname $0`
-ETC_DIR=$SCRIPT_DIR/..
 
 # transfer
-scp -oStrictHostKeyChecking=no -r -i $IDENTITY_FILE $ETC_DIR $AWS_USER@$AWS_HOST:
+scp -oStrictHostKeyChecking=no -r -i $IDENTITY_FILE etc $AWS_USER@$AWS_HOST:
 scp -r -i $IDENTITY_FILE parkandride-application-$VERSION.jar $AWS_USER@$AWS_HOST:parkandride-application-latest.jar
 
 # stop and start docker container
-ssh -i $IDENTITY_FILE -t "$AWS_USER@$AWS_SERVER" "$SCRIPT_DIR/restart.sh"
+ssh -i $IDENTITY_FILE -t "$AWS_USER@$AWS_SERVER" "etc/deploy/restart.sh"

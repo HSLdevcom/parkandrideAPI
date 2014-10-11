75e884ce45546a568318e6ff3cf9acffad2dfd6b
Isto Nikula <isto.nikula@iki.fi>
2014-10-11 16:23:38 +0300

    chore(build): speed up version resolving, v2

--- etc/gocd/build/build-release.sh
+++ etc/gocd/build/build-release.sh
@@ -5,10 +5,9 @@ set -eu
 : ${PWD:?}
 set -x
 
-
-
 function version() {
-  local version=`python -c 'import xml.etree.ElementTree as ET; print ET.parse("pom.xml").find("//{http://maven.apache.org/POM/4.0.0}parent/{http://maven.apache.org/POM/4.0.0}version").text'`
+  local byxpath=".//{http://maven.apache.org/POM/4.0.0}parent/{http://maven.apache.org/POM/4.0.0}version"
+  local version=$(python -c "import xml.etree.ElementTree as ET; print ET.parse('pom.xml').find(\"$byxpath\").text")
   echo $version > version
   sed -i -e "s/SNAPSHOT$/$GO_PIPELINE_COUNTER/" version
   cat version
@@ -18,11 +17,8 @@ function revision() {
   echo `git rev-parse HEAD` > revision
   cat revision
 }
-
 VERSION=`version`
 REVISION=`revision`
-
-exit 0
 
 mvn versions:set \
     --update-snapshots \

828ab94a5e39fd04c2cb63122bfc35991415307f
Isto Nikula <isto.nikula@iki.fi>
2014-10-13 23:20:17 +0300

    test(facility): create hub

--- application/src/main/frontend/protractor/pages/hubs/hubEdit.page.js
+++ application/src/main/frontend/protractor/pages/hubs/hubEdit.page.js
@@ -1,10 +1,37 @@
 var HubEditPage = (function() {
+    var ptor = protractor.getInstance();
+
     function HubEditPage() {
         this.view = element(by.css('.wdHubEditView'));
+        this.name = element(by.model('editCtrl.hub.name'));
+        this.map = element(by.css('.hub-map .ol-viewport'));
+        this.saveButton = element(by.css('.wdSave'));
     }
 
     HubEditPage.prototype.isDisplayed = function() {
         return this.view.isDisplayed();
+    };
+
+    HubEditPage.prototype.setName = function(newName) {
+        return this.name.sendKeys(newName);
+    };
+
+    HubEditPage.prototype.setLocation = function(pos) {
+        ptor.actions()
+            .mouseMove(this.map, {x: pos.x, y: pos.y}).doubleClick()
+            .perform();
+    };
+
+    HubEditPage.prototype.toggleFacility = function(f) {
+        var offsetX = f.border.offset.x + f.border.w/2;
+        var offsetY = f.border.offset.y + f.border.h/2;
+        ptor.actions()
+            .mouseMove(this.map, {x: offsetX, y: offsetY}).click()
+            .perform();
+    };
+
+    HubEditPage.prototype.save = function() {
+        this.saveButton.click();
     };
 
     return HubEditPage;
--- application/src/main/frontend/protractor/specs/basicFlow.spec.js
+++ application/src/main/frontend/protractor/specs/basicFlow.spec.js
@@ -17,8 +17,11 @@ describe('Basic flow', function() {
         return 'Test Facility ' + new Date().getTime();
     }
 
+    function newHubName() {
+        return 'Test Hub ' + new Date().getTime();
+    }
+
     var facility1 = {
-        name: newFacilityName(),
         capacities: {
             "CAR": {"built": 10, "unavailable": 1},
             "BICYCLE": {"built": 20, "unavailable": 2},
@@ -27,15 +30,24 @@ describe('Basic flow', function() {
             "MOTORCYCLE": {"built": 50, "unavailable": 5},
             "ELECTRIC_CAR": {"built": 60, "unavailable": 6}
         },
-        aliases: ["fac1", "facility1"]
+        aliases: ["fac1", "facility1"],
+        border: {
+            offset: {x: 90, y: 90},
+            w: 60,
+            h: 60
+        }
     };
 
     var facility2 = {
-        name: newFacilityName(),
         capacities: {
             "CAR": {"built": 10, "unavailable": 1}
         },
-        aliases: ["fac2"]
+        aliases: ["fac2"],
+        border: {
+            offset: {x: 180, y: 180},
+            w: 60,
+            h: 60
+        }
     };
 
     it('Go to facility create', function() {
@@ -46,10 +58,11 @@ describe('Basic flow', function() {
     });
 
     it('Create facility 1', function () {
+        facility1.name = newFacilityName();
         facilityEditPage.setName(facility1.name);
         expect(facilityEditPage.getName()).toEqual(facility1.name);
 
-        facilityEditPage.drawBorder({x: 60, y: 60}, 60, 60);
+        facilityEditPage.drawBorder(facility1.border.offset, facility1.border.w, facility1.border.h);
         facilityEditPage.addAlias(facility1.aliases[0]);
         facilityEditPage.addAlias(facility1.aliases[1]);
         facilityEditPage.setCapacities(facility1.capacities);
@@ -69,10 +82,11 @@ describe('Basic flow', function() {
     });
 
     it('Create facility 2', function() {
+        facility2.name = newFacilityName();
         facilityEditPage.setName(facility2.name);
         expect(facilityEditPage.getName()).toEqual(facility2.name);
 
-        facilityEditPage.drawBorder({x: 150, y: 150}, 60, 60);
+        facilityEditPage.drawBorder(facility2.border.offset, facility2.border.w, facility2.border.h);
         facilityEditPage.addAlias(facility2.aliases[0]);
         facilityEditPage.setCapacities(facility2.capacities);
 
@@ -88,5 +102,14 @@ describe('Basic flow', function() {
         expect(hubListPage.isDisplayed()).toBe(true);
         hubListPage.toCreateView();
         expect(hubEditPage.isDisplayed()).toBe(true);
+    });
+
+    it('Create hub', function() {
+        var hubName = newHubName();
+        hubEditPage.setName(hubName);
+        hubEditPage.toggleFacility(facility1);
+        hubEditPage.toggleFacility(facility2);
+        hubEditPage.setLocation({x: 165, y: 165});
+        hubEditPage.save();
     });
 });

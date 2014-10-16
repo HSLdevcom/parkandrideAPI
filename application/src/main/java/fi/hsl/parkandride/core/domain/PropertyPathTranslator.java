c6a67f000b94f8213cbf198c48d89af3614a83d1
Isto Nikula <isto.nikula@iki.fi>
2014-10-16 22:27:05 +0300

    refactor(validation): remove recursion and add test cases for translator

--- application/src/main/java/fi/hsl/parkandride/core/domain/PropertyPathTranslator.java
+++ application/src/main/java/fi/hsl/parkandride/core/domain/PropertyPathTranslator.java
@@ -3,13 +3,11 @@ package fi.hsl.parkandride.core.domain;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.springframework.util.StringUtils;
-
 import com.google.common.base.Strings;
 
 public class PropertyPathTranslator {
     private interface RegEx {
-        String HEAD = "[^\\[\\.]+";     // everything up to a [ or .
+        String HEAD = "[^\\[]+";     // everything up to a [
         String INDEX = "\\[(\\w*)\\]";
         String TAIL = "\\.(.*)";        // processed recursively
 
@@ -23,16 +21,22 @@ public class PropertyPathTranslator {
     private static final Pattern PATH = Pattern.compile("(" + RegEx.HEAD + ")(" + RegEx.INDEX + ")(" + RegEx.TAIL + ")*");
 
     public String translate(String input) {
-        if (!Strings.isNullOrEmpty(input)) {
-            Matcher matcher = PATH.matcher(input);
-            if (matcher.matches()) {
-                return new StringBuilder(matcher.group(RegEx.Group.HEAD))
-                        .append(".")
-                        .append(matcher.group(RegEx.Group.INDEX))
-                        .append(".")
-                        .append(translate(matcher.group(RegEx.Group.TAIL)))
-                        .toString();
+        if (!Strings.isNullOrEmpty(input) && input.trim().length() > 0) {
+            String tail = input;
+            StringBuilder translated = new StringBuilder();
+
+            Matcher m = PATH.matcher(tail);
+            while (m.matches()) {
+                translated.append(m.group(RegEx.Group.HEAD)).append(".").append(m.group(RegEx.Group.INDEX));
+
+                tail = m.group(RegEx.Group.TAIL);
+                if (tail == null) {
+                   return translated.toString();
+                }
+                translated.append(".");
+                m = PATH.matcher(tail);
             }
+            return translated.append(tail).toString();
         }
         return input;
     }
--- application/src/test/java/fi/hsl/parkandride/core/domain/PropertyPathTranslatorTest.java
+++ application/src/test/java/fi/hsl/parkandride/core/domain/PropertyPathTranslatorTest.java
@@ -10,15 +10,24 @@ public class PropertyPathTranslatorTest {
     @Test
     public void translates_keys() {
         assertThat(translator.translate("foo[KEY].bar")).isEqualTo("foo.KEY.bar");
+        assertThat(translator.translate("foo[KEY]")).isEqualTo("foo.KEY");
     }
 
     @Test
     public void translates_indices() {
         assertThat(translator.translate("foo[1].bar")).isEqualTo("foo.1.bar");
+        assertThat(translator.translate("foo[1]")).isEqualTo("foo.1");
+    }
+
+    @Test
+    public void translated_multiple_keys_and_indices() {
+        assertThat(translator.translate("foo[1].bar.baz[KEY].qux")).isEqualTo("foo.1.bar.baz.KEY.qux");
+        assertThat(translator.translate("foo[1].bar.baz[KEY]")).isEqualTo("foo.1.bar.baz.KEY");
     }
 
     @Test
     public void no_translation_without_key_or_indice() {
+        assertThat(translator.translate("foo")).isEqualTo("foo");
         assertThat(translator.translate("foo.bar")).isEqualTo("foo.bar");
     }
 

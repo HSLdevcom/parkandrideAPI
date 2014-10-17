79851c9a16f611a757a21fc48fd7fb310c2c6c3b
Isto Nikula <isto.nikula@iki.fi>
2014-10-17 14:17:30 +0300

    chore(validations): wip

--- application/src/main/frontend/build.config.js
+++ application/src/main/frontend/build.config.js
@@ -67,6 +67,7 @@ module.exports = {
       'vendor/placeholders/angular-placeholders-0.0.1-SNAPSHOT.min.js',
       'vendor/angular-ui-router/release/angular-ui-router.js',
       'vendor/angular-ui-utils/modules/route/route.js',
+      'vendor/angular-ui-utils/modules/validate/validate.js',
       'vendor/lodash/dist/lodash.js',
       'vendor/angular-translate/angular-translate.js',
       'vendor/angular-translate-loader-static-files/angular-translate-loader-static-files.js',

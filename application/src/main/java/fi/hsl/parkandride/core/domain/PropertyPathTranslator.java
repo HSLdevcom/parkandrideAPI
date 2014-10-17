27bc0d013b03356c5aa33eff1143ab29838a22bb
Samppa Saarela <samppa.saarela@iki.fi>
2014-10-17 11:22:04 +0300

    refactor(translate): static translate method

--- application/src/main/java/fi/hsl/parkandride/core/domain/PropertyPathTranslator.java
+++ application/src/main/java/fi/hsl/parkandride/core/domain/PropertyPathTranslator.java
@@ -7,14 +7,14 @@ import com.google.common.base.Preconditions;
 public class PropertyPathTranslator {
     private static final Pattern PATTERN = Pattern.compile("\\[(\\D\\w*)\\]");
 
-    public String translate(String input) {
+    public static String translate(String input) {
         Preconditions.checkArgument(input != null);
         Preconditions.checkArgument(!isWhitespaceOnly(input));
 
         return PATTERN.matcher(input).replaceAll(".$1");
     }
 
-    private boolean isWhitespaceOnly(String input) {
+    private static boolean isWhitespaceOnly(String input) {
         return input.length() > 0 && input.trim().isEmpty();
     }
 }
--- application/src/test/java/fi/hsl/parkandride/core/domain/PropertyPathTranslatorTest.java
+++ application/src/test/java/fi/hsl/parkandride/core/domain/PropertyPathTranslatorTest.java
@@ -1,61 +1,61 @@
 package fi.hsl.parkandride.core.domain;
 
+import static fi.hsl.parkandride.core.domain.PropertyPathTranslator.translate;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
 
 public class PropertyPathTranslatorTest {
-    private final PropertyPathTranslator translator = new PropertyPathTranslator();
 
     @Test
     public void translates_keys() {
-        assertThat(translator.translate("foo[KEY].bar")).isEqualTo("foo.KEY.bar");
-        assertThat(translator.translate("foo[KEY]")).isEqualTo("foo.KEY");
+        assertThat(translate("foo[KEY].bar")).isEqualTo("foo.KEY.bar");
+        assertThat(translate("foo[KEY]")).isEqualTo("foo.KEY");
     }
 
     @Test
     public void translates_multiple_keys() {
-        assertThat(translator.translate("foo[1].bar.baz[KEY].qux")).isEqualTo("foo[1].bar.baz.KEY.qux");
-        assertThat(translator.translate("foo[1].bar.baz[KEY]")).isEqualTo("foo[1].bar.baz.KEY");
+        assertThat(translate("foo[1].bar.baz[KEY].qux")).isEqualTo("foo[1].bar.baz.KEY.qux");
+        assertThat(translate("foo[1].bar.baz[KEY]")).isEqualTo("foo[1].bar.baz.KEY");
     }
 
     @Test
     public void no_translation_for_indices() {
-        assertThat(translator.translate("foo[1].bar")).isEqualTo("foo[1].bar");
-        assertThat(translator.translate("foo[1]")).isEqualTo("foo[1]");
+        assertThat(translate("foo[1].bar")).isEqualTo("foo[1].bar");
+        assertThat(translate("foo[1]")).isEqualTo("foo[1]");
     }
 
     @Test
-    public void no_translation_without_key_or_indice() {
-        assertThat(translator.translate("foo")).isEqualTo("foo");
-        assertThat(translator.translate("foo.bar")).isEqualTo("foo.bar");
+    public void no_translation_without_key_or_index() {
+        assertThat(translate("foo")).isEqualTo("foo");
+        assertThat(translate("foo.bar")).isEqualTo("foo.bar");
     }
 
     @Test
     public void no_translation_when_empty() {
-        assertThat(translator.translate("")).isEqualTo("");
+        assertThat(translate("")).isEqualTo("");
     }
 
     @Test
     public void no_translation_when_empty_() {
-        assertThat(translator.translate("foo[.].bar")).isEqualTo("foo...bar");
+        assertThat(translate("foo[.].bar")).isEqualTo("foo...bar");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void nullInputIsIllegal() {
-        translator.translate(null);
+        translate(null);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void whitespaceInputIsIllegal() {
-        translator.translate("  ");
+        translate("  ");
     }
 
     @Test
     public void knownIssues() {
-        assertThat(translator.translate("foo[.].bar")).isEqualTo("foo...bar");
-        assertThat(translator.translate("foo[..].bar")).isEqualTo("foo[..].bar");
-        assertThat(translator.translate("foo[]].bar")).isEqualTo("foo.].bar");
-        assertThat(translator.translate("foo[[].bar")).isEqualTo("foo.[.bar");
+        assertThat(translate("foo[.].bar")).isEqualTo("foo...bar");
+        assertThat(translate("foo[..].bar")).isEqualTo("foo[..].bar");
+        assertThat(translate("foo[]].bar")).isEqualTo("foo.].bar");
+        assertThat(translate("foo[[].bar")).isEqualTo("foo.[.bar");
     }
 }

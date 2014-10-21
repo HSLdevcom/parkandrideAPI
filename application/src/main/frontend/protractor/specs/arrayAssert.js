02f20960e28d1d3718f292e237c77a5d69752ae9
Isto Nikula <isto.nikula@iki.fi>
2014-10-21 16:04:24 +0300

    refactor(arrayAssert): add general promise unwrapper

--- application/src/main/frontend/protractor/specs/arrayAssert.js
+++ application/src/main/frontend/protractor/specs/arrayAssert.js
@@ -4,22 +4,25 @@ module.exports = function () {
     var _ = require('lodash');
     var api = {};
 
-    var assertInOrder = function(actual, expected, opts) {
-        opts = opts || {};
-
-        var toSkip = [];
-        if (opts.allowSkip) {
-            toSkip = _.difference(expected, actual);
+    var unwrap = function (actual, fn) {
+        if (actual instanceof protractor.promise.Promise) {
+            actual.then(function (a) { fn(a); });
+        } else {
+            fn(actual)();
         }
-
-        expect(actual).toEqual(_.difference(expected, toSkip));
-    };
+    }
 
     api.assertInOrder = function(actual, expected, opts) {
-        if (actual instanceof protractor.promise.Promise) {
-            return actual.then(function (a) { assertInOrder(a, expected, opts); });
-        }
-        return assertInOrder(actual, expected, opts);
+        unwrap(actual, function(a) {
+            opts = opts || {};
+
+            var toSkip = [];
+            if (opts.allowSkip) {
+                toSkip = _.difference(expected, a);
+            }
+
+            expect(a).toEqual(_.difference(expected, toSkip));
+        });
     };
 
     return api;

96122e4bc513ca816e2ffbfedf94b8d4d2289502
Samppa Saarela <samppa.saarela@iki.fi>
2014-10-23 17:08:32 +0300

    refactor(hubs): add comment for later reference

--- application/src/main/frontend/src/app/hubs/hubList.js
+++ application/src/main/frontend/src/app/hubs/hubList.js
@@ -33,12 +33,15 @@
     m.controller('HubsCtrl', HubsController);
     function HubsController(HubResource, hubs, facilities) {
         this.hubs = hubs;
-        var attachedFacilityIds = _.sortBy(_.flatten(hubs, "facilityIds"));
+
+        // XXX: Refactor hub+facilities grouping into HubService if it's needed some elsewhere
         this.getFacilities = function(hub) {
             return _.map(hub.facilityIds, function(facilityId) {
                return facilities[facilityId];
             });
         };
+
+        var attachedFacilityIds = _.sortBy(_.flatten(hubs, "facilityIds"));
         this.getUnattachedFacilities = function() {
             return _.filter(facilities, function(facility) {
                 return _.indexOf(attachedFacilityIds, facility.id, true) < 0;

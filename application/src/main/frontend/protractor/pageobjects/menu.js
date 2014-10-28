ce61a75cc98324141ca773025fc8e5044876cb3a
Isto Nikula <isto.nikula@iki.fi>
2014-10-28 09:28:44 +0200

    refactor(pageobject): remove unused selectFacilities from menu

--- application/src/main/frontend/protractor/pageobjects/menu.js
+++ application/src/main/frontend/protractor/pageobjects/menu.js
@@ -6,10 +6,6 @@ module.exports = function(spec) {
     spec.facilities = element.all(by.linkUiSref('facility-list')).first();
     spec.hubs = element.all(by.linkUiSref('hub-list')).first();
 
-    that.selectFacilities = function () {
-        return spec.facilities.click();
-    };
-
     that.toHubs = function () {
         return spec.hubs.click();
     };

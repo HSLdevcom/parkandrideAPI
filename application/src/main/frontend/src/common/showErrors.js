8ed06ddfdf353fa148ce27c27845b3590dd664a6
Isto Nikula <isto.nikula@iki.fi>
2014-11-03 22:10:39 +0200

    refactor(show-errors): clean up directive

--- application/src/main/frontend/src/common/showErrors.js
+++ application/src/main/frontend/src/common/showErrors.js
@@ -1,41 +1,26 @@
 (function() {
-    var showErrorsModule;
+    var m = angular.module('showErrors', []);
 
-    showErrorsModule = angular.module('showErrors', []);
-
-    showErrorsModule.directive('showErrors', function($timeout, $interpolate, showErrorsConfig) {
-        var getShowSuccess, getTrigger, linkFn;
+    m.directive('showErrors', function($interpolate) {
         var errorClass = "validation-error";
-        getTrigger = function(options) {
-            var trigger;
-            trigger = showErrorsConfig.trigger;
-            if (options && (options.trigger != null)) {
-                trigger = options.trigger;
+
+        function linkFn(scope, el, attrs, formCtrl) {
+            var blurred = false;
+
+            function toggleClasses(invalid) {
+                el.toggleClass(errorClass, invalid);
             }
-            return trigger;
-        };
-        getShowSuccess = function(options) {
-            var showSuccess;
-            showSuccess = showErrorsConfig.showSuccess;
-            if (options && (options.showSuccess != null)) {
-                showSuccess = options.showSuccess;
-            }
-            return showSuccess;
-        };
-        linkFn = function(scope, el, attrs, formCtrl) {
-            var blurred, elName, options, showSuccess, toggleClasses, trigger;
-            blurred = false;
-            options = scope.$eval(attrs.showErrors);
-            showSuccess = getShowSuccess(options);
-            trigger = getTrigger(options);
-            elName = $interpolate(el.attr('name'))(scope);
+
+            var elName = $interpolate(el.attr('name'))(scope);
             if (!elName) {
                 throw "show-errors element has no 'name' attribute";
             }
-            el.bind(trigger, function() {
+
+            el.bind('blur', function() {
                 blurred = true;
                 return toggleClasses(formCtrl[elName].$invalid);
             });
+
             scope.$watch(function() {
                 return formCtrl[elName] && formCtrl[elName].$invalid;
             }, function(invalid) {
@@ -44,48 +29,18 @@
                 }
                 return toggleClasses(invalid);
             });
+
             scope.$on('show-errors-check-validity', function() {
-                console.log(elName);
                 return toggleClasses(formCtrl[elName].$invalid);
             });
-            scope.$on('show-errors-reset', function() {
-                return $timeout(function() {
-                    el.removeClass(errorClass);
-                    el.removeClass('has-success');
-                    return blurred = false;
-                }, 0, false);
-            });
-            return toggleClasses = function(invalid) {
-                el.toggleClass(errorClass, invalid);
-                if (showSuccess) {
-                    return el.toggleClass('has-success', !invalid);
-                }
-            };
-        };
+
+            return toggleClasses;
+        }
+
         return {
             restrict: 'A',
             require: '^form',
-            compile: function(elem, attrs) {
-                return linkFn;
-            }
-        };
-    });
-
-    showErrorsModule.provider('showErrorsConfig', function() {
-        var _showSuccess, _trigger;
-        _showSuccess = false;
-        _trigger = 'blur';
-        this.showSuccess = function(showSuccess) {
-            return _showSuccess = showSuccess;
-        };
-        this.trigger = function(trigger) {
-            return _trigger = trigger;
-        };
-        this.$get = function() {
-            return {
-                showSuccess: _showSuccess,
-                trigger: _trigger
-            };
+            link: linkFn
         };
     });
 })();

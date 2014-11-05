3a064ddfea2fe2ef0a5a96808c467d2f256db49c
Samppa Saarela <samppa.saarela@iki.fi>
2014-11-05 13:15:58 +0200

    feat(errors): BindException handling

--- application/src/main/java/fi/hsl/parkandride/front/ExceptionHandlers.java
+++ application/src/main/java/fi/hsl/parkandride/front/ExceptionHandlers.java
@@ -4,26 +4,30 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
-import javax.inject.Inject;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.boot.autoconfigure.web.BasicErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import fi.hsl.parkandride.core.domain.NotFoundException;
+import fi.hsl.parkandride.core.domain.Violation;
 import fi.hsl.parkandride.core.service.ValidationException;
 
 @ControllerAdvice
 public class ExceptionHandlers {
-
-    @Inject
-    BasicErrorController errorController;
 
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(value= NOT_FOUND)
@@ -35,18 +39,48 @@ public class ExceptionHandlers {
     @ExceptionHandler(ValidationException.class)
     @ResponseBody
     public ResponseEntity<Map<String, Object>> validationException(HttpServletRequest request, ValidationException ex) {
-        // NOTE: ResponseEntity.statusCode is immutable and overrides e.g. @ResponseStatus. BasicErrorController uses this attribute.
-        request.setAttribute("javax.servlet.error.status_code", BAD_REQUEST.value());
-        ResponseEntity<Map<String, Object>> responseEntity = errorController.error(request);
-        responseEntity.getBody().put("violations", ex.violations);
-        return responseEntity;
+        return handleError(request, BAD_REQUEST, ex, ex.getMessage(), ex.violations);
+    }
+
+    @ExceptionHandler(BindException.class)
+    @ResponseBody
+    public ResponseEntity<Map<String, Object>> bindException(HttpServletRequest request, BindException ex) {
+        List<Violation> violations = new ArrayList<>();
+        for (FieldError fieldError : ex.getFieldErrors()) {
+            violations.add(new Violation(fieldError.getCode(), fieldError.getField(), fieldError.getDefaultMessage()));
+        }
+        return handleError(request, BAD_REQUEST, ex, "Invalid request parameters", violations);
     }
 
     @ExceptionHandler(RuntimeException.class)
     @ResponseBody
     public ResponseEntity<Map<String, Object>> runtimeException(HttpServletRequest request, RuntimeException ex) {
-        request.setAttribute("javax.servlet.error.status_code", INTERNAL_SERVER_ERROR.value());
-        return errorController.error(request);
+        return handleError(request, INTERNAL_SERVER_ERROR, ex);
+    }
+
+    private ResponseEntity<Map<String, Object>> handleError(HttpServletRequest request, HttpStatus status, Throwable ex) {
+        return handleError(request, status, ex, ex.getMessage(), null);
+    }
+
+   private ResponseEntity<Map<String, Object>> handleError(HttpServletRequest request, HttpStatus status, Throwable ex,
+                                                            String message, List<Violation> violations) {
+        ex = resolveError(ex);
+        Map<String, Object> errorAttributes = new LinkedHashMap<>();
+        errorAttributes.put("status", status.value());
+        errorAttributes.put("message", message);
+        errorAttributes.put("timestamp", new Date());
+        errorAttributes.put("exception", resolveError(ex).getClass().getName());
+        if (violations != null && !violations.isEmpty()) {
+            errorAttributes.put("violations", violations);
+        }
+        return new ResponseEntity<>(errorAttributes, status);
+    }
+
+    private Throwable resolveError(Throwable ex) {
+        while (ex instanceof ServletException && ex.getCause() != null) {
+            ex = ((ServletException) ex).getCause();
+        }
+        return ex;
     }
 
 }

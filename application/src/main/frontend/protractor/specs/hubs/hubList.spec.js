d4b89338103d8c6e2a80f871d97440ba18235400
Isto Nikula <isto.nikula@iki.fi>
2014-11-05 15:27:33 +0200

    test(hub-list): navigation to facility and hub create views

--- application/src/main/frontend/protractor/specs/hubs/hubList.spec.js
+++ application/src/main/frontend/protractor/specs/hubs/hubList.spec.js
@@ -5,9 +5,27 @@ var po = require('../../pageobjects/pageobjects.js');
 describe('hub list', function () {
     var indexPage = po.indexPage({});
     var hubListPage = po.hubListPage({});
+    var hubEditPage = po.hubEditPage({});
+    var facilityEditPage = po.facilityEditPage({});
 
     it('is the default view', function () {
         indexPage.get();
         expect(hubListPage.isDisplayed()).toBe(true);
+    });
+
+    describe('navigation', function () {
+        beforeEach(function () {
+            hubListPage.get();
+        });
+
+        it('to create facility', function () {
+            hubListPage.toFacilityCreateView();
+            expect(facilityEditPage.isDisplayed()).toBe(true);
+        });
+
+        it('to create hub', function () {
+            hubListPage.toHubCreateView();
+            expect(hubEditPage.isDisplayed()).toBe(true);
+        });
     });
 });

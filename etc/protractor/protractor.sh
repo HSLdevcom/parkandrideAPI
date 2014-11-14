d49e0baa21de003da88b6e33d6a2f6e6056694be
Isto Nikula <isto.nikula@iki.fi>
2014-11-14 10:56:12 +0200

    chore(protractor): use main.inc in protractor script

--- etc/protractor/protractor.sh
+++ etc/protractor/protractor.sh
@@ -1,82 +1,48 @@
 #!/bin/bash
 
-fullpath() {
-  pth=$1
-  case $pth in
-    /*)
-      ;;
-    *)
-      pth=`pwd`/$pth
-      ;;
-  esac
-  echo $pth
+function init() {
+  JARFile=${APP_JAR:-"$SCRIPT_DIR/../../application/target/parkandride-application-0.0.1-SNAPSHOT.jar"}
+  PIDFile="$SCRIPT_DIR/application.pid"
+  LOGFile="$SCRIPT_DIR/application.log"
+  NODE_MODULES="$SCRIPT_DIR/node_modules"
+  : ${SERVER_URL:=http://localhost:8080}
+
+  export SERVER_URL
 }
 
-SCRIPT_DIR="`fullpath \`dirname $0\``"
-JARFile=${APP_JAR:-"$SCRIPT_DIR/../../application/target/parkandride-application-0.0.1-SNAPSHOT.jar"}
-PIDFile="$SCRIPT_DIR/application.pid"
-LOGFile="$SCRIPT_DIR/application.log"
-NODE_MODULES="$SCRIPT_DIR/node_modules"
-: ${SERVER_URL:=http://localhost:8080}
-
-export SERVER_URL
-
-log() {
-  echo "$@"
-}
-
-fail() {
-  log "ERROR" "$@"
-  exit 1
-}
-
-print_process() {
+function print_process() {
   echo $(<"$PIDFile")
 }
 
-retryable_condition() {
-  local condition=$1
-  shift
-  local max_duration=$1
-  [ -z $max_duration ] && max_duration=30
-
-  i=0
-  while ! eval "$condition" ; do
-    i=$(($i + 5))
-    if [ $i -gt "$max_duration" ]; then
-      return 1
-    fi
-    sleep 5
-  done
-  true
-}
-
-is_server_up() {
+function is_server_up() {
   log "Polling for server at $SERVER_URL..."
   curl --output /dev/null --silent --head --fail --insecure $SERVER_URL
 }
 
-CMD="$1"; shift
-case "$CMD" in
-  start)
-      java -jar $JARFile --spring.profiles.active=e2e 2>&1 > $LOGFile &
-      ;;
-  wait_until_started)
-      retryable_condition 'is_server_up' 120 || fail "Failed to start application"
-      ;;
-  test)
-      $NODE_MODULES/protractor/bin/webdriver-manager update
-      $NODE_MODULES/protractor/bin/protractor $SCRIPT_DIR/protractor.conf.js "$@" || fail "There are test failures"
-      ;;
-  debug)
-      $NODE_MODULES/protractor/bin/webdriver-manager update
-      $NODE_MODULES/protractor/bin/protractor debug $SCRIPT_DIR/protractor.conf.js "$@" || fail "There are test failures"
-      ;;
-  stop)
-     kill -TERM $(print_process)
-     ;;
-  verify)
-     grep --quiet '<failure' $SCRIPT_DIR/protractor-results.xml && fail "There are test failures"
-esac
+function run() {
+  CMD="$1"; shift
+  case "$CMD" in
+    start)
+        java -jar $JARFile --spring.profiles.active=e2e 2>&1 > $LOGFile &
+        ;;
+    wait_until_started)
+        retryable_condition 'is_server_up' 120 || fail "Failed to start application"
+        ;;
+    test)
+        $NODE_MODULES/protractor/bin/webdriver-manager update
+        $NODE_MODULES/protractor/bin/protractor $SCRIPT_DIR/protractor.conf.js "$@" || fail "There are test failures"
+        ;;
+    debug)
+        $NODE_MODULES/protractor/bin/webdriver-manager update
+        $NODE_MODULES/protractor/bin/protractor debug $SCRIPT_DIR/protractor.conf.js "$@" || fail "There are test failures"
+        ;;
+    stop)
+       kill -TERM $(print_process)
+       ;;
+    verify)
+       grep --quiet '<failure' $SCRIPT_DIR/protractor-results.xml && fail "There are test failures"
+  esac
+}
 
-exit 0
+VERBOSE="true"
+source $(dirname $0)/../scripts/main.inc
--- etc/scripts/dev/clean-frontend.sh
+++ etc/scripts/dev/clean-frontend.sh
@@ -2,16 +2,18 @@
 
 function init() {
   ROOT_DIR=$SCRIPT_DIR/../../..
+
+  FRONTEND_DIR="$ROOT_DIR/application/src/main/frontend"
+  PTOR_DIR="$ROOT_DIR/etc/protractor"
 }
 
 function run() {
-  local frontend_dir="$ROOT_DIR/application/src/main/frontend"
-  rm -rf $frontend_dir/bin
-  rm -rf $frontend_dir/build
-  rm -rf $frontend_dir/vendor
-  rm -rf $frontend_dir/node_modules
+  for i in bin build vendor node_module; do
+    rm -rf $FRONTEND_DIR/$i
+  done
+
+  rm -rf $PTOR_DIR/node_modules
 }
 
 VERBOSE="true"
-
 source $(dirname $0)/../main.inc

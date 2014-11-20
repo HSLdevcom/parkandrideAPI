0947e9ad3389118dda9b5b7cfeb6e4de82321bad
Isto Nikula <isto.nikula@iki.fi>
2014-11-20 14:44:53 +0200

    test(protractor): move placeholder input to last

--- etc/protractor/specs/common.js
+++ etc/protractor/specs/common.js
@@ -3,8 +3,9 @@
 module.exports.capacityTypeOrder = ["Liityntäpysäköinti", "Polkupyörä", "Henkilöauto", "Invapaikka", "Moottoripyörä", "Sähköauto"];
 
 var caps_dont_matter = ["aaa", "Aba", "aca"];
-var v_w = ["bvb", "bwa", "bNotNeededButWeNeed9InputsInHubEditSpec"];
+var v_w = ["bvb", "bwa"];
 var å_ä_ö = ["cåc", "cäa", "cöb"]; // NOTE: on osx this is ordered incorrectly
-module.exports.facilityNameOrder = caps_dont_matter.concat(v_w).concat(å_ä_ö);
+var weNeed9InputsInHubEditSpec = ["ö"];
+module.exports.facilityNameOrder = caps_dont_matter.concat(v_w).concat(å_ä_ö).concat(weNeed9InputsInHubEditSpec);
 
 module.exports.isOsx = /^darwin/.test(process.platform);

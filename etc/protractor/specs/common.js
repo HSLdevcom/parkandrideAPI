9d324f8f60261e2654a3b49abe32c4c05d7281e0
Isto Nikula <isto.nikula@iki.fi>
2014-11-20 14:32:58 +0200

    test(protractor): remove underscore from not needed input

--- etc/protractor/specs/common.js
+++ etc/protractor/specs/common.js
@@ -3,7 +3,7 @@
 module.exports.capacityTypeOrder = ["Liityntäpysäköinti", "Polkupyörä", "Henkilöauto", "Invapaikka", "Moottoripyörä", "Sähköauto"];
 
 var caps_dont_matter = ["aaa", "Aba", "aca"];
-var v_w = ["bvb", "bwa", "b_not_needed_but_we_need_9_inputs_in_hub_edit_spec"];
+var v_w = ["bvb", "bwa", "bNotNeededButWeNeed9InputsInHubEditSpec"];
 var å_ä_ö = ["cåc", "cäa", "cöb"]; // NOTE: on osx this is ordered incorrectly
 module.exports.facilityNameOrder = caps_dont_matter.concat(v_w).concat(å_ä_ö);
 

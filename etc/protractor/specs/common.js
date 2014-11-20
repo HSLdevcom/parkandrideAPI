c0a036953b71c32c18950475af41f8aff8568564
Isto Nikula <isto.nikula@iki.fi>
2014-11-20 11:52:03 +0200

    test(protractor): remove special characters from expected facility names
    The ordering of special character is considered irrelevant.

--- etc/protractor/specs/common.js
+++ etc/protractor/specs/common.js
@@ -1,5 +1,9 @@
 "use strict";
 
 module.exports.capacityTypeOrder = ["Liityntäpysäköinti", "Polkupyörä", "Henkilöauto", "Invapaikka", "Moottoripyörä", "Sähköauto"];
-module.exports.facilityNameOrder = [ "_foo_", "b@z", "Bar", "bär", "foo", "fov", "fow", "fåå", "föö" ];
 
+var caps_dont_matter = ["aaa", "Aba", "aca"];
+var v_w = ["bvb", "bwa", "b_not_needed_but_we_need_9_inputs_in_hub_edit_spec"];
+//var å_ä_ö = ["cåc", "cäa", "cöb"]; // expected
+var å_ä_ö = ["cäa", "cåc", "cöb"]; // invalid but our postgres currently orders it like this, i.e. ä is before å
+module.exports.facilityNameOrder = caps_dont_matter.concat(v_w).concat(å_ä_ö);

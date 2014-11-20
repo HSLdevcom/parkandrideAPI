9c1b4ddabb7372016528da6b157f0f547f43e08f
Isto Nikula <isto.nikula@iki.fi>
2014-11-20 12:27:07 +0200

    test(protractor): fix facility name order
    NOTE: passes on linux, osx order names incorrectly (ä is before å).

--- etc/protractor/specs/common.js
+++ etc/protractor/specs/common.js
@@ -4,6 +4,5 @@ module.exports.capacityTypeOrder = ["Liityntäpysäköinti", "Polkupyörä", "He
 
 var caps_dont_matter = ["aaa", "Aba", "aca"];
 var v_w = ["bvb", "bwa", "b_not_needed_but_we_need_9_inputs_in_hub_edit_spec"];
-//var å_ä_ö = ["cåc", "cäa", "cöb"]; // expected
-var å_ä_ö = ["cäa", "cåc", "cöb"]; // invalid but our postgres currently orders it like this, i.e. ä is before å
+var å_ä_ö = ["cåc", "cäa", "cöb"]; // NOTE: on osx, postgres orders this incorrectly
 module.exports.facilityNameOrder = caps_dont_matter.concat(v_w).concat(å_ä_ö);

185fc2bdfb9b7921961da7a7e916e49da1c25867
Isto Nikula <isto.nikula@iki.fi>
2014-12-03 16:01:35 +0200

    test(rest-api): test error handling, validation exception

--- application/src/test/java/fi/hsl/parkandride/itest/ErrorHandlingITest.java
+++ application/src/test/java/fi/hsl/parkandride/itest/ErrorHandlingITest.java
@@ -1,13 +1,19 @@
 package fi.hsl.parkandride.itest;
 
+import static com.jayway.restassured.RestAssured.given;
 import static com.jayway.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.is;
 
+import java.io.Serializable;
+
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
 
 import com.jayway.restassured.http.ContentType;
+
+import fi.hsl.parkandride.core.domain.Facility;
 
 public class ErrorHandlingITest extends AbstractIntegrationTest {
     @Test
@@ -20,8 +26,20 @@ public class ErrorHandlingITest extends AbstractIntegrationTest {
         ;
     }
 
+    @Test
+    public void validationException() {
+        given()
+            .header("Content-Type", "application/json;charset=UTF-8")
+            .body(new Facility())
+        .when()
+            .post("/api/v1/facilities")
+        .then()
+            .statusCode(HttpStatus.BAD_REQUEST.value())
+            .body("status", is(HttpStatus.BAD_REQUEST.value()))
+            .body("message", is("Invalid data. See violations for details."))
+        ;
+    }
 
-    // validationException
     // bindException
     // jsonException
     // methodNotSupportedException

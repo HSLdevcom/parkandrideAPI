e3e765894ff6feed6021f35727295c18beaef71f
Isto Nikula <isto.nikula@iki.fi>
2014-12-04 15:04:28 +0200

    test(rest-api): test error handling, HttpMessageNotReadableException

--- application/src/test/java/fi/hsl/parkandride/itest/ErrorHandlingITest.java
+++ application/src/test/java/fi/hsl/parkandride/itest/ErrorHandlingITest.java
@@ -3,10 +3,18 @@ package fi.hsl.parkandride.itest;
 import static com.jayway.restassured.RestAssured.given;
 import static com.jayway.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.startsWith;
+
+import java.io.IOException;
 
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
 
 import fi.hsl.parkandride.core.domain.Facility;
 import fi.hsl.parkandride.core.service.ValidationException;
@@ -37,10 +45,10 @@ public class ErrorHandlingITest extends AbstractIntegrationTest {
     }
 
     @Test
-    public void jsonException() {
+    public void httpMessageNotReadableException() {
         given()
             .header("Content-Type", "application/json;charset=UTF-8")
-            .body("{ \"name\": \"foo\", \"location\": \"bar\"  }")
+            .body("{ \"name\": \"foo\", \"location\": \"invalid location\"  }")
         .when()
             .post("/api/v1/facilities")
         .then()
@@ -50,7 +58,24 @@ public class ErrorHandlingITest extends AbstractIntegrationTest {
         ;
     }
 
-    // bindException
-    // methodNotSupportedException
-    // exception
+    @Test
+    public void httpRequestMethodNotSupportedException() {
+        given()
+            .log().all()
+            .header("Content-Type", "application/json;charset=UTF-8")
+            .body(new Facility())
+        .when()
+            .put("api/v1/facilities")
+        .then()
+            .log().all()
+            .statusCode(HttpStatus.BAD_REQUEST.value())
+            .body("status", is(HttpStatus.BAD_REQUEST.value()))
+            .body("exception", is(HttpRequestMethodNotSupportedException.class.getCanonicalName()))
+        ;
+    }
+
+    // HttpMessageNotReadableException_jsonMappingException: unclear how to trigger
+    // HttpMediaTypeException: unclear how to trigger
+    // bindException: unclear how to trigger
+    // exception: unclear how to trigger
 }

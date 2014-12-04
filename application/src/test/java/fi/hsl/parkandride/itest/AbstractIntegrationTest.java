4b6412c2457eb0809e837635639a327876bc985d
Isto Nikula <isto.nikula@iki.fi>
2014-12-04 15:55:50 +0200

    test(rest-api): move common integration test stuff to base class

--- application/src/test/java/fi/hsl/parkandride/itest/AbstractIntegrationTest.java
+++ application/src/test/java/fi/hsl/parkandride/itest/AbstractIntegrationTest.java
@@ -1,14 +1,26 @@
 package fi.hsl.parkandride.itest;
 
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+import java.io.IOException;
+
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
 import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.builder.RequestSpecBuilder;
+import com.jayway.restassured.builder.ResponseSpecBuilder;
+import com.jayway.restassured.specification.RequestSpecification;
+import com.jayway.restassured.specification.ResponseSpecification;
 
 import fi.hsl.parkandride.Application;
 
@@ -25,4 +37,26 @@ public abstract class AbstractIntegrationTest {
         RestAssured.port = port;
     }
 
+
+    public static String resourceAsString(String resourcePath) {
+        try {
+            return Resources.toString(Resources.getResource(resourcePath), Charsets.UTF_8);
+        } catch (IOException e) {
+            throw new AssertionError("Loading of resource '" + resourcePath + "' failed: " + e);
+        }
+    }
+
+    public static RequestSpecification givenWithContent() {
+        return given().spec(new RequestSpecBuilder()
+                .addHeader("Content-Type", "application/json;charset=UTF-8")
+                .build());
+    }
+
+    public static ResponseSpecification assertResponse(HttpStatus status, Class<?> exClass) {
+        return new ResponseSpecBuilder()
+                .expectStatusCode(status.value())
+                .expectBody("status", is(status.value()))
+                .expectBody("exception", is(exClass.getCanonicalName()))
+                .build();
+    }
 }
--- application/src/test/java/fi/hsl/parkandride/itest/ErrorHandlingITest.java
+++ application/src/test/java/fi/hsl/parkandride/itest/ErrorHandlingITest.java
@@ -1,6 +1,5 @@
 package fi.hsl.parkandride.itest;
 
-import static com.jayway.restassured.RestAssured.given;
 import static com.jayway.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.is;
 
@@ -10,13 +9,6 @@ import org.junit.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
-
-import com.google.common.base.Charsets;
-import com.google.common.io.Resources;
-import com.jayway.restassured.builder.RequestSpecBuilder;
-import com.jayway.restassured.builder.ResponseSpecBuilder;
-import com.jayway.restassured.specification.RequestSpecification;
-import com.jayway.restassured.specification.ResponseSpecification;
 
 import fi.hsl.parkandride.core.domain.Facility;
 import fi.hsl.parkandride.core.service.ValidationException;
@@ -47,7 +39,7 @@ public class ErrorHandlingITest extends AbstractIntegrationTest {
     @Test
     public void httpMessageNotReadableException() {
         givenWithContent()
-            .body("{ \"name\": \"foo\", \"location\": \"invalid location\"  }")
+            .body("{ \"name\": \"foo\", \"location\": \"this is not readable location\"  }")
         .when()
             .post(UrlSchema.FACILITIES)
         .then()
@@ -58,11 +50,10 @@ public class ErrorHandlingITest extends AbstractIntegrationTest {
     @Test
     public void httpMessageNotReadableException_jsonMappingException() throws IOException {
         givenWithContent()
-            .body(Resources.toString(Resources.getResource("facility.create.JsonMappingException.json"), Charsets.UTF_8))
+            .body(resourceAsString("facility.create.JsonMappingException.json"))
         .when()
             .post(UrlSchema.FACILITIES)
         .then()
-            .log().all()
             .spec(assertResponse(HttpStatus.BAD_REQUEST, HttpMessageNotReadableException.class))
         ;
     }
@@ -82,18 +73,4 @@ public class ErrorHandlingITest extends AbstractIntegrationTest {
     // HttpMediaTypeException: unclear how to trigger
     // bindException: unclear how to trigger
     // exception: unclear how to trigger
-
-    private static RequestSpecification givenWithContent() {
-        return given().spec(new RequestSpecBuilder()
-                .addHeader("Content-Type", "application/json;charset=UTF-8")
-                .build());
-    }
-
-    private static ResponseSpecification assertResponse(HttpStatus status, Class<?> exClass) {
-        return new ResponseSpecBuilder()
-                .expectStatusCode(status.value())
-                .expectBody("status", is(status.value()))
-                .expectBody("exception", is(exClass.getCanonicalName()))
-                .build();
-    }
 }

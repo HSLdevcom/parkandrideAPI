49f71af56d2ffadf6018935b5b1268f307d0e923
Isto Nikula <isto.nikula@iki.fi>
2014-12-17 16:18:07 +0200

    chore(authentication): don't generate stack traces for auth failures

--- application/src/main/java/fi/hsl/parkandride/core/service/AuthenticationRequiredException.java
+++ application/src/main/java/fi/hsl/parkandride/core/service/AuthenticationRequiredException.java
@@ -1,4 +1,7 @@
 package fi.hsl.parkandride.core.service;
 
 public class AuthenticationRequiredException extends RuntimeException {
+    public AuthenticationRequiredException() {
+        super(null, null, true, false);
+    }
 }

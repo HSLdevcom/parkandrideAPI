d043501598b8dd81d7bbb15d0611b67cd50dbb5d
Isto Nikula <isto.nikula@iki.fi>
2014-12-23 09:52:48 +0200

    feat(protractor): generate screenshot report

--- etc/protractor/specs/common.js
+++ etc/protractor/specs/common.js
@@ -9,27 +9,3 @@ var weNeed9InputsInHubEditSpec = ["ö"];
 module.exports.facilityNameOrder = caps_dont_matter.concat(v_w).concat(å_ä_ö).concat(weNeed9InputsInHubEditSpec);
 
 module.exports.isOsx = /^darwin/.test(process.platform);
-
-
-var fs = require('fs');
-
-function spec() {
-    return jasmine.getEnv().currentSpec;
-}
-
-function capture() {
-    var name = spec().description.split(' ').join('_');
-    browser.takeScreenshot().then(function(png){
-        var stream = fs.createWriteStream(name + '.png');
-        stream.write(new Buffer(png, 'base64'));
-        stream.end();
-    });
-}
-
-afterEach(function () {
-    if (!spec().results().passed()) {
-        capture();
-    }
-});
-
-module.exports.takeScreenshot = function() { capture(); };

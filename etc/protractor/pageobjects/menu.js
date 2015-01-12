9014bc8aaa7d8e80816fb71d09f3e29ab93a4c72
Isto Nikula <isto.nikula@iki.fi>
2015-01-12 16:49:40 +0200

    fix(tabs): add menu spec

--- etc/protractor/pageobjects/menu.js
+++ etc/protractor/pageobjects/menu.js
@@ -3,11 +3,36 @@
 module.exports = function(spec) {
     var that = require('./base')(spec);
 
-    spec.facilities = element.all(by.linkUiSref('facility-list')).first();
     spec.hubs = element.all(by.linkUiSref('hub-list')).first();
+    spec.contacts = element.all(by.linkUiSref('contact-list')).first();
+    spec.operators = element.all(by.linkUiSref('operator-list')).first();
+
+    spec.isActive = function(link) {
+        return spec.hasClasses(spec.parent(link), ['active']);
+    };
 
     that.toHubs = function () {
         return spec.hubs.click();
+    };
+
+    that.toContacts = function () {
+        return spec.contacts.click();
+    };
+
+    that.toOperators = function () {
+        return spec.operators.click();
+    };
+
+    that.isHubsActive = function() {
+        return spec.isActive(spec.hubs);
+    };
+
+    that.isContactsActive = function() {
+        return spec.isActive(spec.contacts);
+    };
+
+    that.isOperatorsActive = function() {
+        return spec.isActive(spec.operators);
     };
 
     return that;

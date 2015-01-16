1b66a5fdc564255cf7169cc52a1ce93126d1eb20
Isto Nikula <isto.nikula@iki.fi>
2015-01-16 14:25:25 +0200

    fix(payment-info): require payment info url and detail in all languages (if any)
    Url and detail is not required in general, but if any data is provided, it must be provided in all languages.

--- application/src/main/java/fi/hsl/parkandride/core/domain/Facility.java
+++ application/src/main/java/fi/hsl/parkandride/core/domain/Facility.java
@@ -49,6 +49,7 @@ public class Facility implements OperatorEntity {
     @Valid
     public FacilityContacts contacts = new FacilityContacts();
 
+    @Valid
     public FacilityPaymentInfo paymentInfo = new FacilityPaymentInfo();
 
 

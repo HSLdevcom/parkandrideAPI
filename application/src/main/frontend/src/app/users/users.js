614ec20630aeccf801d2a49821f40bb68d826198
Isto Nikula <isto.nikula@iki.fi>
2015-01-26 09:27:45 +0200

    refactor(users): extract user modal into a module

--- application/src/main/frontend/src/app/users/users.js
+++ application/src/main/frontend/src/app/users/users.js
@@ -5,6 +5,7 @@
         'parkandride.operators',
         'parkandride.UserResource',
         'parkandride.layout',
+        'parkandride.users.userModal',
         'showErrors'
     ]);
 
@@ -55,116 +56,6 @@
             restrict: 'E',
             templateUrl: 'users/userListNavi.tpl.html'
         };
-    });
-
-    m.factory('userModal', function($modal, OperatorResource) {
-        return {
-            open: function(user) {
-                return $modal.open({
-                    templateUrl: 'users/userModal.tpl.html',
-                    controller: 'UserModalCtrl as ctrl',
-                    resolve: {
-                        user: function () {
-                            return _.cloneDeep(user);
-                        },
-                        operators: function() {
-                            return OperatorResource.listOperators();
-                        }
-                    },
-                    backdrop: 'static'
-                });
-            }
-        };
-    });
-    m.controller('UserModalCtrl', function($scope, $modalInstance, $translate, schema, user, EVENTS, UserResource, operators, Session) {
-        var vm = this;
-        var isNewUser = !user;
-
-        vm.titleKey = 'users.action.' + (isNewUser ? 'new' : 'edit');
-        vm.user = user || {};
-        vm.save = save;
-        vm.cancel = cancel;
-        vm.roles = translatedEnumValues("roles", schema.roles);
-        vm.operators = operators.results;
-        vm.onOperatorSelect = onOperatorSelect;
-        vm.onRoleSelect = onRoleSelect;
-        vm.isOperatorCreator = false;
-        vm.isPasswordRequired = isPasswordRequired;
-        vm.isAdmin = isAdmin;
-
-        initialize();
-
-        function initialize() {
-            var login = Session.get();
-            if (login && login.operatorId) {
-                vm.isOperatorCreator = true;
-
-                vm.operators = _.filter(vm.operators, function(o) { return o.id === login.operatorId; });
-                vm.user.operatorId = login.operatorId;
-
-                vm.roles = _.filter(vm.roles, function(r) { return !isAdmin(r.id); });
-            }
-        }
-
-        function isAdmin(role) { return (role || vm.user.role) === 'ADMIN'; }
-        function isApi() { return vm.user.role === 'OPERATOR_API';}
-
-        function isPasswordRequired() {
-            return isNewUser && !isApi();
-        }
-
-        function onOperatorSelect(item, model) {
-            if (isAdmin()) {
-                vm.user.role = undefined;
-            }
-        }
-
-        function onRoleSelect(item, model) {
-            if (isAdmin(model)) {
-                vm.user.operatorId = undefined;
-            }
-        }
-
-        function save(form) {
-            validateAndSubmit(form, function() {
-                UserResource.save(vm.user).then(handleSubmitSuccess, handleSubmitReject);
-            });
-        }
-
-        function cancel() {
-            $modalInstance.dismiss();
-        }
-
-        function translatedEnumValues(prefix, values) {
-            return _.map(values, function(v) {
-                return {
-                    id: v,
-                    label: $translate.instant(prefix + "." + v + ".label")
-                };
-            });
-        }
-
-        function handleSubmitSuccess(success) {
-            $modalInstance.close(success);
-        }
-
-        function handleSubmitReject(rejection) {
-            if (rejection.status == 400 && rejection.data.violations) {
-                $scope.violations = rejection.data.violations;
-            }
-        }
-
-        function validateAndSubmit(form, saveFn) {
-            $scope.$broadcast(EVENTS.showErrorsCheckValidity);
-            if (form.$valid) {
-                saveFn();
-            } else {
-                $scope.violations = [{
-                    path: "",
-                    type: "BasicRequirements"
-                }];
-            }
-        }
     });
 
 })();

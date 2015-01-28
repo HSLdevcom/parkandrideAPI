6f431947ce458d3b82fca5141455d44f2b9ba5d8
Isto Nikula <isto.nikula@iki.fi>
2015-01-28 17:07:53 +0200

    test(users-e2e): operator does not see admin nor other operator's users

--- etc/protractor/specs/users/users.spec.js
+++ etc/protractor/specs/users/users.spec.js
@@ -20,11 +20,24 @@ describe('users', function () {
     var operatorY = operator({ name: { fi: "y-operator", sv: "y-operator", en: "y-operator"}});
 
     var admin = user({ username: "admin", password: "admin_pass", role: "ADMIN"});
-    var operatorX_user = user({ username: "operatorx_user", password: "operatorX_user_pass", role: "OPERATOR", operatorId: operatorX.id });
-    var operatorX_api = user({ username: "operatorx_api", role: "OPERATOR_API", operatorId: operatorX.id});
-    var operatorY_user = user({ username: "operatory_user", password: "operatorY_user_pass", role: "OPERATOR", operatorId: operatorY.id});
+    var operatorX_user = user({ username: "operator_x_user", password: "operator_x_user_pass", role: "OPERATOR", operatorId: operatorX.id });
+    var operatorX_api = user({ username: "operator_x_api", role: "OPERATOR_API", operatorId: operatorX.id});
+    var operatorY_user = user({ username: "operator_y_user", password: "operator_y_user_pass", role: "OPERATOR", operatorId: operatorY.id});
 
     var usersPage = po.usersPage({});
+    var menu = po.menu({});
+    var authModal = po.authModal({});
+
+    function toUsersAs(user) {
+        menu.toHubs(); // required for the users view to refresh on logout/login
+
+        menu.canLogout().then(function(canLogout) { if (canLogout) menu.logout(); });
+        menu.openLoginModal();
+        authModal.login(user.username,  user.password);
+        
+        menu.toUsers();
+        expect(usersPage.isDisplayed()).toBe(true);
+    }
 
     describe('navigation', function () {
         beforeEach(function () {
@@ -39,7 +52,6 @@ describe('users', function () {
         
         // TODO: edit pending, let's see it this is even needed
     });
-
 
     describe('lists users', function () {
         beforeEach(function () {
@@ -70,6 +82,21 @@ describe('users', function () {
                 expect(actual[3].username).toEqual(operatorY_user.username);
                 expect(actual[3].operator).toEqual(operatorY.name.fi);
                 expect(actual[3].role).toEqual("operaattori");
+            });
+        });
+
+        it("operator does not see admin nor other operator's users", function() {
+            toUsersAs(operatorX_user);
+            usersPage.getUsers().then(function (actual){
+                expect(actual.length).toBe(2);
+
+                expect(actual[0].username).toEqual(operatorX_user.username);
+                expect(actual[0].operator).toEqual(operatorX.name.fi);
+                expect(actual[0].role).toEqual("operaattori");
+
+                expect(actual[1].username).toEqual(operatorX_api.username);
+                expect(actual[1].operator).toEqual(operatorX.name.fi);
+                expect(actual[1].role).toEqual("API");
             });
         });
     });

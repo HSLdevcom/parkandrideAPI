5710d94185b0e0fc75bff22702592b6b7ee1f216
Isto Nikula <isto.nikula@iki.fi>
2015-02-04 16:07:09 +0200

    test(users): use valid password in user tests

--- etc/protractor/specs/users/users.spec.js
+++ etc/protractor/specs/users/users.spec.js
@@ -8,27 +8,28 @@ var devApi = require('../devApi')();
 var common = require('../common');
 
 describe('users', function () {
+    function createOperator(object) {
+        object.id = ++seq;
+        object.toPayload = function() { return this; };
+        return object;
+    }
 
-    function create(object) {
+    function createUser(object) {
         object.id = ++seq;
         object.toPayload = function() { return this; };
 
-        if (object.role) {
-            if (object.role === 'ADMIN') {
-                object.roleFi = 'Ylläpitäjä';
-                if (object.username) {
-                    object.password = object.username;
-                } else {
-                    object.username = object.password = 'admin';
-                }
-            }
-            if (object.role === 'OPERATOR') object.roleFi = 'Operaattori';
-            if (object.role === 'OPERATOR_API') object.roleFi = 'API';
+        object.roleFi = roleNameFi[object.role];
+        if (object.role !== "OPERATOR_API") {
+            object.password = defaultPassword;
+        }
 
-            if (object.operatorId) {
-                object.operator = operators[object.operatorId];
-                object.username = object.password =  object.operator.name.fi + object.id
-            }
+        if (object.role === 'ADMIN') {
+            object.username = object.username || "admin";
+        }
+
+        if (object.operatorId) {
+            object.operator = operators[object.operatorId];
+            object.username = object.operator.name.fi + object.id;
         }
 
         return object;
@@ -36,14 +37,21 @@ describe('users', function () {
 
     var seq = 0;
 
-    var operatorX = create({ name: { fi: "x-operator", sv: "x-operator", en: "x-operator"}});
-    var operatorY = create({ name: { fi: "y-operator", sv: "y-operator", en: "y-operator"}});
+    var defaultPassword = "paSs1234";
+    var roleNameFi = {
+        "ADMIN": "Ylläpitäjä",
+        "OPERATOR": "Operaattori",
+        "OPERATOR_API": "API"
+    };
+
+    var operatorX = createOperator({ name: { fi: "x-operator", sv: "x-operator", en: "x-operator"}});
+    var operatorY = createOperator({ name: { fi: "y-operator", sv: "y-operator", en: "y-operator"}});
     var operators = _.indexBy([operatorX, operatorY], "id");
 
-    var admin = create({ role: "ADMIN"});
-    var operatorX_user = create({ role: "OPERATOR", operatorId: operatorX.id });
-    var operatorX_api = create({ role: "OPERATOR_API", operatorId: operatorX.id});
-    var operatorY_user = create({ role: "OPERATOR", operatorId: operatorY.id});
+    var admin = createUser({ role: "ADMIN"});
+    var operatorX_user = createUser({ role: "OPERATOR", operatorId: operatorX.id });
+    var operatorX_api = createUser({ role: "OPERATOR_API", operatorId: operatorX.id});
+    var operatorY_user = createUser({ role: "OPERATOR", operatorId: operatorY.id});
 
     var usersPage = po.usersPage({});
     var menu = po.menu({});
@@ -125,7 +133,7 @@ describe('users', function () {
         });
 
         it('on enter all fields are shown', function () {
-            expect(usersPage.userModal.getRoles()).toEqual(['Ylläpitäjä', 'Operaattori', 'API']);
+            expect(usersPage.userModal.getRoles()).toEqual([roleNameFi.ADMIN, roleNameFi.OPERATOR, roleNameFi.OPERATOR_API]);
             expect(usersPage.userModal.canSetUsername()).toBe(true);
             expect(usersPage.userModal.canSetPassword()).toBe(true);
             expect(usersPage.userModal.getOperators()).toEqual([operatorX.name.fi, operatorY.name.fi]);
@@ -133,20 +141,20 @@ describe('users', function () {
 
         it('when admin role is selected, operator is hidden', function () {
             expect(usersPage.userModal.canSetOperator()).toBe(true);
-            usersPage.userModal.setRole('Ylläpitäjä');
+            usersPage.userModal.setRole(roleNameFi.ADMIN);
             expect(usersPage.userModal.canSetOperator()).toBe(false);
         });
 
         it('when api role is selected, password is hidden', function () {
             expect(usersPage.userModal.canSetPassword()).toBe(true);
-            usersPage.userModal.setRole('API');
+            usersPage.userModal.setRole(roleNameFi.OPERATOR_API);
             expect(usersPage.userModal.canSetPassword()).toBe(false);
         });
 
         it('added user is shown on the list', function () {
-            var newadmin = create({ role: "ADMIN", username: "newadmin"});
+            var newadmin = createUser({ role: "ADMIN", username: "newadmin"});
 
-            usersPage.userModal.setRole('Ylläpitäjä');
+            usersPage.userModal.setRole(roleNameFi.ADMIN);
             usersPage.userModal.setUsername(newadmin.username);
             usersPage.userModal.setPassword(newadmin.password);
             usersPage.userModal.save();
@@ -179,13 +187,13 @@ describe('users', function () {
         });
 
         it('cannot create admin', function () {
-            expect(usersPage.userModal.getRoles()).toEqual(['Operaattori', 'API']);
+            expect(usersPage.userModal.getRoles()).toEqual([roleNameFi.OPERATOR, roleNameFi.OPERATOR_API]);
         });
 
         it('can create operator', function () {
-            var newoperator = create({ role: "OPERATOR", username: "newoperator", operatorId: operatorX.id});
+            var newoperator = createUser({ role: "OPERATOR", username: "newoperator", operatorId: operatorX.id});
 
-            usersPage.userModal.setRole('Operaattori');
+            usersPage.userModal.setRole(roleNameFi.OPERATOR);
             usersPage.userModal.setUsername(newoperator.username);
             usersPage.userModal.setPassword(newoperator.password);
             usersPage.userModal.save();
@@ -201,9 +209,9 @@ describe('users', function () {
         });
 
         it('can create operator api', function () {
-            var newoperator = create({ role: "OPERATOR_API", username: "newoperatorapi", operatorId: operatorX.id});
+            var newoperator = createUser({ role: "OPERATOR_API", username: "newoperatorapi", operatorId: operatorX.id});
 
-            usersPage.userModal.setRole('API');
+            usersPage.userModal.setRole(roleNameFi.OPERATOR_API);
             usersPage.userModal.setUsername(newoperator.username);
             usersPage.userModal.save();
 

71c5e5c94f0a4761dd6ab185d28e189ebf3af995
Esko Luontola <esko.luontola@gmail.com>
2015-03-12 14:16:06 +0200

    chore(deploy): add hsl-liipi key to ssh-agent

--- etc/scripts/gocd/deploy/deploy.sh
+++ etc/scripts/gocd/deploy/deploy.sh
@@ -8,9 +8,14 @@ VERSION=`cat version`
 BINARY=`readlink -f staging/fi/hsl/parkandride/parkandride-application/$VERSION/parkandride-application-$VERSION.jar`
 
 cd deploy
+
 echo '#!/bin/bash
 set -eu
 echo "$VAULT_PASSWORD"
 ' > vault-password
 chmod a+x vault-password
+
+eval `ssh-agent -s`
+ssh-add "$HOME/hsl-liipi.pem"
+
 ./ansible-playbook site.yml -l "$ENV" -e "app_binary=$BINARY"

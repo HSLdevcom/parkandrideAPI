d46ad2e3e65b6063b67d7748c1452617cf0f2d4a
Esko Luontola <esko.luontola@gmail.com>
2015-03-12 10:56:04 +0200

    chore(deploy): vault pass file

--- etc/scripts/gocd/deploy/deploy.sh
+++ etc/scripts/gocd/deploy/deploy.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -eu
 : ${ENV:?}
+: ${VAULT_PASS:?}
 set -x
 
 VERSION=`cat version`
 BINARY=`readlink -f staging/fi/hsl/parkandride/parkandride-application/$VERSION/parkandride-application-$VERSION.jar`
 
 cd deploy
+echo '#!/bin/bash
+set -eu
+echo "$VAULT_PASS"
+' > vault-pass
+chmod a+x vault-pass
 ./ansible-playbook site.yml -l "$ENV" -e "app_binary=$BINARY"

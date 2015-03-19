25ab0b19b7939efaa9d4281718f7771fd13cd57b
Samppa Saarela <samppa.saarela@iki.fi>
2015-03-19 15:06:57 +0200

    docs(utilization): all Utilization fields are required

--- application/src/main/java/fi/hsl/parkandride/core/domain/Utilization.java
+++ application/src/main/java/fi/hsl/parkandride/core/domain/Utilization.java
@@ -6,21 +6,26 @@ package fi.hsl.parkandride.core.domain;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
+import com.wordnik.swagger.annotations.ApiModelProperty;
 import org.joda.time.DateTime;
 
 public class Utilization {
 
     @NotNull
+    @ApiModelProperty(required = true)
     public DateTime timestamp;
 
     @NotNull
+    @ApiModelProperty(required = true)
     public CapacityType capacityType;
 
     @NotNull
+    @ApiModelProperty(required = true)
     public Usage usage;
 
     @NotNull
     @Min(0)
+    @ApiModelProperty(required = true)
     public Integer spacesAvailable;
 
 }

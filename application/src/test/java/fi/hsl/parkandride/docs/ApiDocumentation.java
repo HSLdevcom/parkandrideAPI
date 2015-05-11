03c5bd59c7677a6ed331b636e0ddbe0792b0e513
Esko Luontola <esko.luontola@gmail.com>
2015-05-11 14:35:15 +0300

    docs(api): facility example, wip

--- application/src/test/java/fi/hsl/parkandride/docs/ApiDocumentation.java
+++ application/src/test/java/fi/hsl/parkandride/docs/ApiDocumentation.java
@@ -11,6 +11,7 @@ import fi.hsl.parkandride.front.UrlSchema;
 import fi.hsl.parkandride.itest.AbstractIntegrationTest;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.boot.test.IntegrationTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.config.RestDocumentationConfigurer;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,9 +23,13 @@ import javax.inject.Inject;
 
 import static fi.hsl.parkandride.core.domain.Role.OPERATOR_API;
 import static org.springframework.restdocs.RestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@IntegrationTest("spring.jackson.serialization.indent_output:true")
 public class ApiDocumentation extends AbstractIntegrationTest {
 
     @Inject Dummies dummies;
@@ -55,6 +60,33 @@ public class ApiDocumentation extends AbstractIntegrationTest {
         mockMvc.perform(request)
                 .andExpect(status().isOk())
                 .andDo(document("authentication-example"));
+    }
+
+    @Test
+    public void facilityExample() throws Exception {
+        MockHttpServletRequestBuilder request = get(UrlSchema.FACILITY, facilityId);
+        this.mockMvc.perform(request)
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("facility-example")
+                        .withResponseFields(
+                                fieldWithPath("id").description("TODO"),
+                                fieldWithPath("name").description("TODO"),
+                                fieldWithPath("location").description("TODO"),
+                                fieldWithPath("operatorId").description("TODO"),
+                                fieldWithPath("status").description("TODO"),
+                                fieldWithPath("pricingMethod").description("TODO"),
+                                fieldWithPath("statusDescription").description("TODO"),
+                                fieldWithPath("builtCapacity").description("Built capacity by CapacityType, may be split or shared by different Usage types as defined by pricing"),
+                                fieldWithPath("usages").description("Read-only summary of distinct pricing rows' usages"),
+                                fieldWithPath("pricing").description("TODO, deep"),
+                                fieldWithPath("unavailableCapacities").description("TODO, deep"),
+                                fieldWithPath("aliases").description("TODO"),
+                                fieldWithPath("ports").description("TODO, deep"),
+                                fieldWithPath("services").description("TODO"),
+                                fieldWithPath("contacts").description("TODO, deep"),
+                                fieldWithPath("paymentInfo").description("TODO, deep"),
+                                fieldWithPath("openingHours").description("TODO, deep")));
     }
 
 

401ad9780655db3d0376059e8c30dfd43b7fb331
Samppa Saarela <samppa.saarela@iki.fi>
2015-06-12 10:00:24 +0300

    fix(docs): Null in summary of facility model

--- application/src/test/java/fi/hsl/parkandride/back/Dummies.java
+++ application/src/test/java/fi/hsl/parkandride/back/Dummies.java
@@ -50,7 +50,7 @@ public class Dummies {
     @TransactionalWrite
     public long createFacility() {
         Long operatorId = createDummyOperator();
-        FacilityContacts contacts = new FacilityContacts(createDummyContact(), createDummyContact());
+        FacilityContacts contacts = new FacilityContacts(createDummyContact(), createDummyContact(), createDummyContact());
         Facility facility = createFacility(operatorId, contacts);
         return facilityDao.insertFacility(facility);
     }
@@ -100,6 +100,10 @@ public class Dummies {
 
         facility.openingHours.info = new MultilingualString("Opening Hours");
         facility.openingHours.url = new MultilingualUrl("http://www.hsl.fi");
+
+        facility.paymentInfo.detail = new MultilingualString("Payment details");
+        facility.paymentInfo.url = new MultilingualUrl("http://www.hsl.fi");
+
         return facility;
     }
 

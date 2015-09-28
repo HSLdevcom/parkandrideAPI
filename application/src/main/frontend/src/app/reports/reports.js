96c61e139367cbe7a5b556365b971fccb193e6d9
Reko Jokelainen <reko.jokelainen@nitorcreations.com>
2015-09-28 09:49:17 +0300

    feat(report): filter facilities by region, hub, and operator

--- application/src/main/frontend/src/app/reports/reports.js
+++ application/src/main/frontend/src/app/reports/reports.js
@@ -48,26 +48,105 @@
     });
 
     m.controller('ReportsCtrl', function ($scope, $translate, $http, allOperators, allHubs, allFacilities, allRegions, schema) {
-        $scope.allOperators = allOperators;
-        $scope.allHubs = allHubs;
-        $scope.allFacilities = allFacilities;
-        $scope.allRegions = allRegions;
-        $scope.capacityTypes = schema.capacityTypes.values;
-        $scope.usages = schema.usages.values;
+        $scope.allOperators     = allOperators;
+        $scope.allRegions       = allRegions;
+        $scope.allHubs          = allHubs;
+        $scope.allFacilities    = allFacilities;
+        $scope.capacityTypes    = schema.capacityTypes.values;
+        $scope.usages           = schema.usages.values;
         var contentType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
-        var date = new Date();
 
+        //
+        // UTILS
+        //
+        var date = new Date();
         function to_date(daynumber) {
             return daynumber + '.' + (date.getMonth() + 1) + '.' + date.getFullYear();
         }
 
+        //
+        // REPORT DEFAULTS
+        //
         $scope.reportType = 'FacilityUsage';
         $scope.report = {
             startDate: to_date(1),
             endDate: to_date(date.getDate()),
             interval: 60,
-            capacityTypes: ['CAR']
+            capacityTypes: ['CAR'],
+            regions: [],
+            hubs: [],
+            operators: []
         };
+
+        //
+        // UI FILTERS
+        //
+
+        function isBlank(arr) {
+            return !arr || arr.length === 0; 
+        }
+
+        function ifNotEmpty(arr) {
+            return {
+                then: function(cb) {
+                    return function actualFilter(val) {
+                        return isBlank(arr) ? true : cb(val);
+                    };
+                }
+            };
+        }
+
+        function getPermittedHubs(regionIds) {
+            return _.chain(regionIds)
+                .map(function (id) { return _.find($scope.allRegions, 'id', id); })
+                .map(function(region) { return region.hubIds; })
+                .flatten()
+                .map(function (hubId) { return _.find($scope.allHubs, 'id', hubId); })
+                .value();
+        }
+
+        var permittedHubs = $scope.allHubs;
+        var permittedFacilities = $scope.allFacilities;
+        $scope.$watchCollection('[report.regions, report.hubs, report.operators]', function(arr) {
+            var regionIds = arr[0] || [];
+            var hubIds = arr[1] || [];
+            var operatorIds = arr[2] || [];
+
+            // Util functions
+            var bySelectedHubIds = function (hub) { return hubIds.indexOf(hub.id) !== -1; };
+            var bySelectedOperatorIds = function (hub) { return operatorIds.indexOf(hub.operatorId) !== -1; };
+            var toFacilityIds = function (hub) { return hub.facilityIds; };
+            var toFacility = function(id) { return _.find($scope.allFacilities, 'id', id); };
+
+            // Permitted hubs by regions
+            permittedHubs = regionIds.length === 0 ? $scope.allHubs : getPermittedHubs(regionIds);
+
+            // Facilities are filtered by regions, hubs, and operators
+            if ([].concat(operatorIds, hubIds, regionIds).length > 0) {
+                // If any filters set
+                permittedFacilities = _.chain(permittedHubs)
+                    .filter(ifNotEmpty(hubIds).then(bySelectedHubIds))
+                    .map(toFacilityIds)
+                    .flatten()
+                    .map(toFacility)
+                    .filter(ifNotEmpty(operatorIds).then(bySelectedOperatorIds))
+                    .value();
+            } else {
+                permittedFacilities = $scope.allFacilities;
+            }
+        });
+
+        $scope.isPermittedHub = function(hub) {
+            return permittedHubs.indexOf(hub) !== -1;
+        };
+
+        $scope.isPermittedFacility = function(facility) {
+            return permittedFacilities.indexOf(facility) !== -1;
+        };
+
+        //
+        // REPORT GENERATION
+        //
         $scope.generate = function (type, parameters) {
             if (!parameters) {
                 parameters = {};

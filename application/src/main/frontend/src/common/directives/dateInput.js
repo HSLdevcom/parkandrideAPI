2a2d2a9bb91c54af656d83009c0c50f7ea8ce121
Reko Jokelainen <reko.jokelainen@nitorcreations.com>
2015-10-20 12:51:01 +0300

    fixup! chore(deps): update angular-bootstrap to 0.14.0

--- application/src/main/frontend/src/common/directives/dateInput.js
+++ application/src/main/frontend/src/common/directives/dateInput.js
@@ -10,15 +10,16 @@
         format: 'd.M.yyyy'
     });
 
-    m.constant('datepickerConfig', {
-        startingDay: 1,
-        yearRange: 5,
-        minDate: new Date(2010, 0, 1, 0, 0, 0)
-    });
-
-    m.constant('datepickerPopupConfig', {
-        showButtonBar: false,
-        datepickerPopup: 'd.M.yyyy'
+    m.config(function(uibDatepickerConfig, uibDatepickerPopupConfig, dateInputConfig) {
+        _.extend(uibDatepickerConfig, {
+            startingDay: 1,
+            yearRange: 5,
+            minDate: new Date(2010, 0, 1, 0, 0, 0)
+        });
+        _.extend(uibDatepickerPopupConfig, {
+            showButtonBar: false,
+            datepickerPopup: dateInputConfig.format
+        });
     });
 
     /**
@@ -32,13 +33,13 @@
                 function parseDate(val) {
                     return uibDateParser.parse(val, dateInputConfig.format);
                 }
-                // Replace the formatters and parsers of datepicker-popup for good
-                ngModel.$formatters = [function(val) {
+
+                ngModel.$formatters.unshift(function(val) {
                     return dateFilter(val, dateInputConfig.format);
-                }];
-                ngModel.$parsers = [function(val) {
+                });
+                ngModel.$parsers.unshift(function(val) {
                     return parseDate(val);
-                }];
+                });
 
                 ngModel.$validators.date = function(modelValue, viewValue) {
                     return !!parseDate(viewValue);

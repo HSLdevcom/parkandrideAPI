d2e61d56afb1802a30b15ad83d68f168adb81fe6
Esko Luontola <esko@luontola.fi>
2018-06-28 18:27:08 +0300

    refactor(build): package the frontend with nginx

--- application/src/main/frontend/build.sh
+++ application/src/main/frontend/build.sh
@@ -4,6 +4,7 @@ set -eu
 readonly CACHE_FILE=".yarn-cache.tgz"
 readonly CACHED_LOCK_FILE=".yarn-cache.lock"
 readonly IMAGE="liipi-web"
+readonly BUILDER_IMAGE="${IMAGE}-builder"
 
 # initialize an empty cache file
 if [[ ! -f "${CACHE_FILE}" ]]; then
@@ -12,13 +13,18 @@ if [[ ! -f "${CACHE_FILE}" ]]; then
 fi
 
 # do the build
-docker build --tag "${IMAGE}:latest" .
+echo "### Build builder image ###"
+docker build --tag "${BUILDER_IMAGE}" --target builder .
+echo
+echo "### Build production image ###"
+docker build --tag "${IMAGE}" .
 
 # update cache if dependencies have changed
+touch -a "${CACHED_LOCK_FILE}"
 cached_lock=$(cat "${CACHED_LOCK_FILE}")
-latest_lock=$(docker run --rm "${IMAGE}" cat /tmp/yarn.lock)
+latest_lock=$(docker run --rm "${BUILDER_IMAGE}" cat /tmp/yarn.lock)
 if [[ "${cached_lock}" != "${latest_lock}" ]]; then
     echo "Update ${CACHE_FILE} from current build"
-    docker run --rm "${IMAGE}" tar -czf - /home/node/.cache > "${CACHE_FILE}"
+    docker run --rm "${BUILDER_IMAGE}" tar -czf - /home/node/.cache > "${CACHE_FILE}"
     echo "${latest_lock}" > "${CACHED_LOCK_FILE}"
 fi

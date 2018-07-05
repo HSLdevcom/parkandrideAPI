68354d19a9d03aa5c36a0a03999a3c14ebb462de
Esko Luontola <esko@luontola.fi>
2018-07-05 15:57:56 +0300

    refactor(build): simpler docker caching strategy using labels

---
+++ cache-builder-images.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -eu
+
+for name in "liipi-api-builder-cache" "liipi-web-builder-cache"; do
+    latest_id=$(docker images --filter "label=cache-this-layer=${name}" --format "{{.ID}}" | head -n 1)
+    docker tag "${latest_id}" "${name}"
+    echo "Tagged ${latest_id} as ${name}"
+done

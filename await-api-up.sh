ad3719d56f9cf2c0f510c0120a8954bc7636aff5
Esko Luontola <esko@luontola.fi>
2018-07-10 12:30:54 +0300

    refactor(build): limit on waiting, print last error

--- await-api-up.sh
+++ await-api-up.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
+set -u
 
-until $(curl --output /dev/null --silent --fail http://localhost:8080/api/v1/hubs); do
-    sleep 1
+tries=0
+max_tries=20
+interval=1
+
+while true; do
+    output=$(curl --max-time 5 --silent --verbose --fail http://localhost:8080/api/v1/hubs 2>&1)
+    status=$?
+    if [[ "$status" -eq 0 ]]; then
+        exit 0
+    fi
+    ((tries++))
+    if [[ "$tries" -ge "$max_tries" ]]; then
+        echo "$output"
+        exit 1
+    fi
+    sleep "$interval"
 done

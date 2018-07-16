0d5c772bb963fdbd53d4ab27952a84c05e41df13
Esko Luontola <esko@luontola.fi>
2018-07-16 13:21:32 +0300

    refactor(build): publish docker images

--- publish.sh
+++ publish.sh
@@ -7,7 +7,7 @@ readonly REGISTRY="995219551342.dkr.ecr.eu-west-1.amazonaws.com"
 readonly API_IMAGE="liipi-api"
 readonly WEB_IMAGE="liipi-web"
 
-export AWS_PROFILE=liipi
+#export AWS_PROFILE=liipi
 eval $(aws ecr get-login --no-include-email --region eu-west-1)
 
 set -x
